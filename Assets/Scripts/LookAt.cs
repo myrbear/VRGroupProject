2e465b71e0312d888b55cf13065f54efd919bde8
mzlafty <mzlafty22@my.trine.edu>
2024-03-29 00:16:57 -0400

    environment still stilli in progress:

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -6,6 +6,9 @@ public class LookAt : MonoBehaviour
 {
     [SerializeField] private Transform m_Target;
     [SerializeField] private Vector3 m_Offset;
+    [SerializeField] private float m_RotateSpeed = 4f;
+    [SerializeField] private float m_RadiusHorizontal;
+    [SerializeField] private float m_RadiusVertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,30 @@ public class LookAt : MonoBehaviour
     void FixedUpdate()
     {
         transform.LookAt(m_Target);
-        transform.position = m_Target.position + m_Target.rotation * m_Offset;
+
+        float vertical = Input.GetKey(KeyCode.UpArrow) ? 1 : 0;
+        if (vertical == 0)
+        {
+            vertical = Input.GetKey(KeyCode.DownArrow) ? -1 : 0;
+        }
+        float horizontal = Input.GetKey(KeyCode.RightArrow) ? 1 : 0;
+        if (horizontal == 0)
+        {
+            horizontal = Input.GetKey(KeyCode.LeftArrow) ? -1 : 0;
+        }
+        if (horizontal != 0)
+        {
+            m_RadiusHorizontal -= horizontal * Time.fixedDeltaTime * m_RotateSpeed;
+        }
+        if (vertical != 0)
+        {
+            m_RadiusVertical += vertical * Time.fixedDeltaTime * m_RotateSpeed;
+        }
+
+
+        transform.position = m_Target.position
+            + Mathf.Cos(m_RadiusHorizontal) * new Vector3(1, 0, 0) * 10f
+            + Mathf.Sin(m_RadiusHorizontal) * new Vector3(0, 0, 1) * 10
+            + Mathf.Cos(m_RadiusVertical) * new Vector3(0, 1, 0) * 10f;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,30 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float m_JumpVelocity = 5f;
     private Rigidbody m_Rigidbody;
+    private Transform m_Camera;
 
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Camera = GameObject.Find("Camera").transform;
     }
 
     private void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        m_Rigidbody.velocity = transform.rotation * new Vector3(horizontal * m_Speed, m_Rigidbody.velocity.y, vertical * m_Speed);
+        float vertical = Input.GetKey(KeyCode.W) ? 1 : 0;
+        if (vertical == 0)
+        {
+            vertical = Input.GetKey(KeyCode.S) ? -1 : 0;
+        }
+        float horizontal = Input.GetKey(KeyCode.D) ? 1 : 0;
+        if (horizontal == 0)
+        {
+            horizontal = Input.GetKey(KeyCode.A) ? -1 : 0;
+        }
+        Quaternion rotation = m_Camera.rotation.normalized;
+        rotation.x = 0;
+        rotation.z = 0;
+        m_Rigidbody.velocity = rotation * new Vector3(horizontal * m_Speed * Time.deltaTime, m_Rigidbody.velocity.y, vertical * m_Speed * Time.deltaTime);
 
 
     }
